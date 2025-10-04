194203b3120a5a58b540f87dab50c3c79f974dfd
zeynep kılınç <zeynepkilinc771@gmail.com>
2025-10-04 22:14:43 +0300

    Add files via upload
    asal sayı sorgulama.

---
+++ Asalsayi.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package asalsayi;
+import java.util.*;
+public class Asalsayi {
+
+    public static void main(String[] args) {
+        int sayac,i,j,n=10;
+        System.out.print("asal sayilari bulmak istediginiz n sayisini giriniz: ");
+        //Scanner klavye=new Scanner(System.in);
+       // n=klavye.nextInt();
+        for(i=2;i<=n;i++){
+            sayac=0;
+            for(j=2;j<i;j++){
+                if(i%j==0){
+                    sayac++;
+                }
+            }
+            if(sayac==0){
+                System.out.print(i+" ");
+            }
+        }
+       
+    }
+    
+}
